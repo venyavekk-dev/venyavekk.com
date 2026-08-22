6c5d0c5cf38edfaac573db7bbfdb6110d77814d2
Venya Vekk <venyavekk@gmail.com>
2026-08-22 15:16:41 +0200

    Simplify track preview icons

--- components/sections/CollectionSection.tsx
+++ components/sections/CollectionSection.tsx
@@ -130,18 +130,16 @@ function MusicItem({
           >
             <img src={item.image} width={44} height={44} alt="" className="music-row-cover" />
             <span className="music-preview-icon" aria-hidden="true">
-              <span className="music-preview-control">
-                {isPreviewPlaying ? (
-                  <svg className="music-preview-glyph" viewBox="0 0 24 24">
-                    <rect x="7" y="5.5" width="3.5" height="13" rx="1.25" />
-                    <rect x="13.5" y="5.5" width="3.5" height="13" rx="1.25" />
-                  </svg>
-                ) : (
-                  <svg className="music-preview-glyph is-play" viewBox="0 0 24 24">
-                    <path d="M8.5 6.35c0-.78.86-1.26 1.53-.85l9.05 5.65a1 1 0 0 1 0 1.7l-9.05 5.65a1 1 0 0 1-1.53-.85V6.35Z" />
-                  </svg>
-                )}
-              </span>
+              {isPreviewPlaying ? (
+                <svg className="music-preview-glyph" viewBox="0 0 24 24">
+                  <rect x="7" y="5.5" width="3.5" height="13" rx="1.25" />
+                  <rect x="13.5" y="5.5" width="3.5" height="13" rx="1.25" />
+                </svg>
+              ) : (
+                <svg className="music-preview-glyph is-play" viewBox="0 0 24 24">
+                  <path d="M8.5 6.35c0-.78.86-1.26 1.53-.85l9.05 5.65a1 1 0 0 1 0 1.7l-9.05 5.65a1 1 0 0 1-1.53-.85V6.35Z" />
+                </svg>
+              )}
             </span>
             <audio
               ref={audioRef}
